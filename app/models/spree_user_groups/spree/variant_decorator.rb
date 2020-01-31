7c1a453afe847b9392f22ac898c8fc8c39e769f8
Nemo Fang <fyj123121@gmail.com>
2020-01-31 15:11:10 +0200

    Remove the price_in overwrite method.

--- app/models/spree_user_groups/spree/variant_decorator.rb
+++ app/models/spree_user_groups/spree/variant_decorator.rb
@@ -9,23 +9,6 @@ module SpreeUserGroups::Spree::VariantDecorator
       default_price
     end
   end
-
-  def price_in(currency)
-    @price_in ||= {}
-    return @price_in[currency] if @price_in[currency]
-
-    currency ||= Spree::Config[:currency]
-    price = prices.select{ |price| price.currency == currency }.first || Spree::Price.new(variant_id: self.id, currency: currency)
-    new_amount = price.amount
-
-    if Spree::User.current && Spree::User.current.user_group
-      price_for_user = Spree::User.current.user_group.price_for_variant(self, price)
-      new_amount = price_for_user if price_for_user
-    end
-
-    # This line caused exception in old version. Variant_id can not be nil.
-    @price_in[currency] = Spree::Price.new(variant_id: self.id, amount: new_amount, currency: currency)
-  end
 end
 
 ::Spree::Variant.prepend SpreeUserGroups::Spree::VariantDecorator
